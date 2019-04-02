4c63e52f9dd6841261f381043fdfb2c178633d5f
2628748861 <2628748861@qq.com>
2019-04-02 17:13:49 +0800

    修改缓存2x

--- library/src/main/java/com/plugin/foundation/library/app/ApplicationProxy.java
+++ library/src/main/java/com/plugin/foundation/library/app/ApplicationProxy.java
@@ -59,7 +59,7 @@ public abstract class ApplicationProxy extends Application
 
 
     protected IApplifeCycle provideHttpConifg(){
-        return new HttpDelegate("",false);
+        return new HttpDelegate(this,"",false);
     }
     protected abstract IApplifeCycle provideDatabaseConifg();
     protected IApplifeCycle provideImageLoaderConifg()
