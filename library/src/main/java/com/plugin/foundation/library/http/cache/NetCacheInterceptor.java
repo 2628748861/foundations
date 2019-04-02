4e0be9a4df148266f7ca314201275b1773621dcf
2628748861 <2628748861@qq.com>
2019-04-02 16:56:11 +0800

    修改http缓存数据2

--- library/src/main/java/com/plugin/foundation/library/http/cache/NetCacheInterceptor.java
+++ library/src/main/java/com/plugin/foundation/library/http/cache/NetCacheInterceptor.java
@@ -13,19 +13,18 @@ public class NetCacheInterceptor implements Interceptor
     @Override
     public Response intercept(Chain chain) {
         Request request = chain.request();
-        CacheControl cacheControl = new CacheControl.Builder()
-                .maxAge(60, TimeUnit.SECONDS)
-                .maxStale(120, TimeUnit.SECONDS).build();
+//        CacheControl cacheControl = new CacheControl.Builder()
+//                .maxAge(60, TimeUnit.SECONDS)
+//                .maxStale(120, TimeUnit.SECONDS).build();
+        request = request.newBuilder().cacheControl(CacheControl.FORCE_CACHE).build();
         Response response = null;
         try {
             response = chain.proceed(request);
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return response.newBuilder()
-                .removeHeader("Pragma")
-                .removeHeader("Cache-Control")
-                .header("Cache-Control", cacheControl.toString())
-                .build();
+
+
+        return response;
     }
 }
