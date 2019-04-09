04bed48cf2b0eeafeb793ac4ee5eebd84d6bb920
2628748861 <2628748861@qq.com>
2019-04-09 15:08:48 +0800

    修改activity和application样式

--- library/src/main/java/com/plugin/foundation/library/mvp/activity/BaseMvpActivity.java
+++ library/src/main/java/com/plugin/foundation/library/mvp/activity/BaseMvpActivity.java
@@ -1,11 +1,21 @@
 package com.plugin.foundation.library.mvp.activity;
 
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
 import com.plugin.foundation.library.mvp.presenter.MvpPresenter;
 import com.plugin.foundation.library.mvp.view.MvpView;
 import com.plugin.foundation.library.mvp.view.delegate.DefaultViewDelegateImp;
 
 public abstract class BaseMvpActivity<Presenter extends MvpPresenter> extends MvpActivity<Presenter> {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //用于切除application样式的影响
+        getWindow().setBackgroundDrawableResource(android.R.color.white);
+    }
+
     @Override
     protected MvpView applyViewDelegate() {
         return new DefaultViewDelegateImp(this,contentView);
